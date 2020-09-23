c76b1b8c744b186826166e374afa1ceac069910d
Sierra Harris <sierra.harris154@outlook.com>
2020-09-23 09:08:08 -0700

    Updated to include Precipitation route.

--- app.py
+++ app.py
@@ -39,17 +39,25 @@ session = Session(engine)
 app = Flask(__name__)
 
 @app.route('/')
-
 def welcome():
     return(
     '''
     Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end<br/>
     ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the precipitation data for the last year"""
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
 if __name__ == '__main__':
     app.run()
